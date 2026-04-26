9887c63e14443b770d91e7b638db93a7db569637
Bashitha267 <nimeshspc2k17@gmail.com>
2026-04-26 23:45:42 +0530

    added kogout function

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -86,7 +86,17 @@ async function login(req, res) {
   }
 }
 
+async function logout(req, res) {
+  try {
+    res.clearCookie('token');
+    return res.status(200).json({ message: 'Logout successful' });
+  } catch (error) {
+    return res.status(500).json({ message: 'Failed to logout', error: error.message });
+  }
+}
+
 module.exports = {
   registerOwner,
   login,
+  logout,
 };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { registerOwner, login } = require('../controllers/auth/auth.controller');
+const { registerOwner, login, logout } = require('../controllers/auth/auth.controller');
 
 const router = express.Router();
 
 router.post('/register', registerOwner);
 router.post('/login', login);
+router.post('/logout', logout);
 
 module.exports = router;
