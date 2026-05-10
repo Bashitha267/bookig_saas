6e89b01e992a54d66ffae14384575edbb606ab06
Bashitha267 <nimeshspc2k17@gmail.com>
2026-05-10 16:07:25 +0530

    gg

--- index.js
+++ index.js
@@ -11,28 +11,7 @@ const roomRoutes = require('./routes/room.routes');
 const bookingRoutes = require('./routes/booking.routes');
 const paymentRoutes = require('./routes/payment.routes');
 
-const corsOrigins = [
-  process.env.CLIENT_URL,
-  /^https:\/\/.*\.hostingersite\.com$/
-].filter(Boolean);
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) {
-      return callback(null, true);
-    }
-    const isAllowed = corsOrigins.some((entry) => {
-      if (entry instanceof RegExp) {
-        return entry.test(origin);
-      }
-      return entry === origin;
-    });
-    return callback(null, isAllowed);
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(cors());
 app.options('*', cors());
 app.use(express.json());
 
